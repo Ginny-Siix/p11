01a90b8a3cd2589b45fc13312cab1722344a02af
Ginny-Siix <127752611+Ginny-Siix@users.noreply.github.com>
2023-12-21 20:37:05 +0100

    la boulette

--- https---github.com-Ginny-Siix-https---github.com-Ginny-Siix-projet-10-main/src/composants/Feature/Feature.jsx
+++
@@ -1,9 +0,0 @@
-export default function Feature({ logo, altText, title, description }) {
-  return (
-    <article className="feature">
-      <img src={logo} alt={altText} className="feature-icon" />
-      <h3 className="feature-title">{title}</h3>
-      <p>{description}</p>
-    </article>
-  );
-}
